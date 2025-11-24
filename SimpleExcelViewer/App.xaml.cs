5e6ee31eed500a247b749369cc726d44a00fe0f2
R.Wolfer <rainbowwolfer@outlook.com>
2025-11-24 17:36:25 +0800

    Update

--- SimpleExcelViewer/App.xaml.cs
+++ SimpleExcelViewer/App.xaml.cs
@@ -1,14 +1,54 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using RW.Base.WPF;
+using RW.Base.WPF.Configs;
+using RW.Base.WPF.Extensions;
+using RW.Base.WPF.ViewModels;
+using System.Diagnostics;
+using System.Globalization;
 using System.Windows;
 
-namespace SimpleExcelViewer
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+namespace SimpleExcelViewer;
 
+public partial class App : ApplicationBase {
+	private static App? instance;
+	public static App Instance => instance!;
+
+	static App() {
+		DebugConfig.Print = o => Debug.WriteLine(o);
+		DebugConfig.DebuggerBreak = Debugger.Break;
+	}
+
+	public override bool IsRelease => AppConfig.IsRelease;
+
+	public App() {
+		instance = this;
+	}
+
+	protected override Window GetMainWindow() => new MainWindow();
+
+	protected override CultureInfo? GetCultureInfo() {
+		// todo
+		return null;
+	}
+
+	protected override AppManager GetAppManager() => new _AppManager();
+	protected override DllLoader GetDllLoader() => new _DllLoader();
+	protected override IoCInitializer GetIoCInitializer() => new _IoCInitializer(DllLoader);
+
+	protected override void ShowFatalDialog(Exception exception) {
+		MessageBox.Show(exception.ToString(), "Fatal Error");
+	}
+
+
+	private class _AppManager : AppManager {
+		public override string AppName => AppConfig.AppName;
+		public override string BuildMode => AppConfig.IsRelease ? "Release" : "Debug";
+	}
+
+	private class _DllLoader : DllLoader {
+
+	}
+
+	private class _IoCInitializer(DllLoader dllLoader) : IoCInitializer(dllLoader) {
+
+	}
 }
---
+++ SimpleExcelViewer/AppConfig.cs
@@ -0,0 +1,41 @@
+﻿using RW.Common.Models;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace SimpleExcelViewer;
+
+public static class AppConfig
+{
+
+	public const string Author = "RainbowWolfer";
+	public const string AppName = "SimpleExcelViewer";
+
+
+	public static bool IsRelease
+	{
+		get
+		{
+#if RELEASE
+			return true;
+#else
+			return false;
+#endif
+		}
+	}
+
+	public static Guid SessionID { get; }
+	public static DateTime AppStartTime { get; }
+	public static VersionStruct Version { get; }
+
+	static AppConfig()
+	{
+		SessionID = Guid.NewGuid();
+		AppStartTime = DateTime.Now;
+
+		string location = Assembly.GetEntryAssembly()!.Location;
+		FileVersionInfo info = FileVersionInfo.GetVersionInfo(location);
+		Version = new VersionStruct(info.FileMajorPart, info.ProductMinorPart, info.FileBuildPart, info.FilePrivatePart);
+
+		Debug.WriteLine(location);
+	}
+}

d4e25804c0bf05204aadb9b9ebebb4f27ac0a4bb
R.Wolfer <rainbowwolfer@outlook.com>
2025-12-04 18:42:02 +0800

    Update

---
+++ SimpleExcelViewer/Services/AppStatusService.cs
@@ -0,0 +1,32 @@
+﻿using DevExpress.Mvvm;
+using RW.Base.WPF.DependencyInjections;
+using RW.Common;
+
+namespace SimpleExcelViewer.Services;
+
+public interface IAppStatusService {
+
+	event TypedEventHandler<IAppStatusService, EventArgs>? OnFullScreenChanged;
+
+	bool FullScreen { get; set; }
+
+}
+
+internal class AppStatusService : BindableBase, IAppStatusService, ISingletonDependency {
+	public event TypedEventHandler<IAppStatusService, EventArgs>? OnFullScreenChanged;
+
+	public bool FullScreen {
+		get => GetProperty(() => FullScreen);
+		set {
+			SetProperty(() => FullScreen, value);
+			OnFullScreenChanged?.Invoke(this, EventArgs.Empty);
+		}
+	}
+
+
+
+	public AppStatusService() {
+		FullScreen = false;
+	}
+
+}
--- SimpleExcelViewer/Views/MainWindow.xaml.cs
+++ SimpleExcelViewer/Views/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using DevExpress.Mvvm;
 using RW.Base.WPF.Events;
 using RW.Base.WPF.Interfaces;
+using RW.Base.WPF.ViewModelServices;
 using SimpleExcelViewer.Controls;
 using SimpleExcelViewer.Events;
+using SimpleExcelViewer.Services;
+using System.Windows;
 using System.Windows.Input;
 
 namespace SimpleExcelViewer.Views;
@@ -13,12 +16,56 @@ public partial class MainWindow : WindowBase {
 	}
 }
 
-internal class MainWindowViewModel(IEventAggregator eventAggregator, IApplication application) : ViewModelBase {
+internal class MainWindowViewModel(
+	IEventAggregator eventAggregator,
+	IApplication application,
+	IAppStatusService appStatusService
+) : ViewModelBase {
+
+	private ICurrentWindowServiceEx CurrentWindowService => GetService<ICurrentWindowServiceEx>();
+
+	private WindowState previousWindowState = WindowState.Normal;
+
+	protected override void OnInitializeInRuntime() {
+		base.OnInitializeInRuntime();
+
+		appStatusService.OnFullScreenChanged += AppStatusService_OnFullScreenChanged;
+	}
+
+
+	private DelegateCommand<EventArgs>? stateChangedCommand;
+	public IDelegateCommand StateChangedCommand => stateChangedCommand ??= new(StateChanged);
+	private void StateChanged(EventArgs args) {
+		//if (appStatusService.FullScreen) {
+		//	appStatusService.FullScreen = false;
+		//}
+	}
+
+	private void AppStatusService_OnFullScreenChanged(IAppStatusService sender, EventArgs args) {
+		Window window = CurrentWindowService.GetWindow();
+		if (sender.FullScreen) {
+			previousWindowState = window.WindowState;
+			window.WindowState = WindowState.Maximized;
+			window.WindowStyle = WindowStyle.None;
+		} else {
+			window.WindowState = previousWindowState;
+			window.WindowStyle = WindowStyle.SingleBorderWindow;
+		}
+	}
 
 	private DelegateCommand<KeyEventArgs>? previewKeyDownCommand;
 	public IDelegateCommand PreviewKeyDownCommand => previewKeyDownCommand ??= new(PreviewKeyDown);
 	private void PreviewKeyDown(KeyEventArgs args) {
+
+		if (args.Key == Key.F11) {
+			args.Handled = true;
+			appStatusService.FullScreen = !appStatusService.FullScreen;
+			return;
+		}
+
+
 		eventAggregator.GetEvent<MainWindowPreviewKeyDownEvent>().Publish(args);
+
 	}
 
 
